3e742853f9e173df08f9de037370e28713e979d0
Rajeev <s2100155@edu.bc.fi>
2021-12-14 13:02:40 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Todo} from "./todo";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'test';
+  
+
+  todoValue!: string; // Add (!) sign after name: 
+  list!: Todo[];
+
+ ngOnInit() {
+   this.list = [];
+   this.todoValue = "";
+ }
+ addItem() {
+   if (this.todoValue !== "") {
+     const newItem: Todo = {
+       id: Date.now(),
+       value: this.todoValue,
+       isDone:false
+     };
+     this.list.push(newItem);
+   }
+   this.todoValue = "";
+ }
+ deleteItem(id:number) {
+   this.list = this.list.filter(item => item.id !== id);
+ }
 }
